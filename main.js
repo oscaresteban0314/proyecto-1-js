50531d3e95ecd38266008e63c1470c3415b9db87
oscaresteban0314 <oatenas78@gmail.com>
2023-01-18 21:39:46 -0500

    events desde js

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ const num1  = document.querySelector('#num1');
 const num2  = document.querySelector('#num2');
 const result = document.querySelector('#result');
 const resultp = document.querySelector('#resultp');
-function sumar() {
-    let n1 =Number(num1.value);
+const form = document.querySelector('#form');
+const btn = document.querySelector('#result');
+console.log(form);
+/* tn =addEventListener('click',sumar); */
+form = addEventListener('submit', sumar);
+/* num2.value=addEventListener('change',sumar)
+num1.value=addEventListener('change',sumar) */
+resultp.innerText="";
+function sumar(event) {
+    event.preventDefault();
     let n2 =Number(num2.value);
+    let n1 =Number(num1.value);
     let suma = n1 + n2;
-    resultp.innerText = "la suma de "+n1+"+"+n2+"="+suma
-    return; 
+    resultp.innerText = "Resultado: "+suma
 }
